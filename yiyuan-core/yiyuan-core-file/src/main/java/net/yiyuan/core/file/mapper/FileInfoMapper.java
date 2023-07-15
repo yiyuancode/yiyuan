ff4d0406445ebdbf1b743c45ce5d2d1d3c70a60d
小林同学 <505675821@qq.com>
2023-07-15 21:46:16 +0800

    文件上传部分

---
+++ yiyuan-core/yiyuan-core-file/src/main/java/net/yiyuan/core/file/mapper/FileInfoMapper.java
@@ -0,0 +1,16 @@
+package net.yiyuan.core.file.mapper;
+
+import net.yiyuan.core.file.model.FileInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import icu.mhb.mybatisplus.plugln.base.mapper.JoinBaseMapper;
+import net.yiyuan.common.*;
+
+
+/**
+ * Mapper接口
+ *
+ * @author 一源团队--小林同学
+ * @date 2023-07-15
+ */
+public interface FileInfoMapper extends JoinBaseMapper<FileInfo> {
+}
