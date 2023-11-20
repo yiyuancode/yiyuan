24fcdb162e4648939f8d6f17380ff9cd27cd0b6d
xl <505675821@qq.com>
2023-11-20 22:21:18 +0800

    行政区域查询

--- yiyuan-sys/yiyuan-sys-app-api/src/main/java/net/yiyuan/controller/SysAreaAppController.java
+++ yiyuan-sys/yiyuan-sys-app-api/src/main/java/net/yiyuan/controller/SysAreaAppController.java
@@ -1,9 +1,17 @@
 package net.yiyuan.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import net.yiyuan.common.model.vo.CommonResult;
 import net.yiyuan.service.SysAreaAppService;
+import net.yiyuan.vo.SysAreaQueryVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotBlank;
+import java.util.List;
 
 /**
  * 行政区域移动端接口
@@ -16,4 +24,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SysAreaAppController {
   @Autowired private SysAreaAppService sysAreaAppService;
+
+  /**
+   * 行政区域查询
+   *
+   * @return {@link CommonResult <List<SysAreaQueryVO>>}
+   * @author ${author}
+   * @date 2023-07-27
+   */
+  @GetMapping(value = "/sys/area/Query/{pid}")
+  @ResponseBody
+  public CommonResult<List<SysAreaQueryVO>> getUmUserInfo(@PathVariable("pid")  String pid) throws Exception {
+    return CommonResult.success(sysAreaAppService.getAreaTree(pid), "获取用户信息成功");
+  }
+
 }
--- yiyuan-sys/yiyuan-sys-app-service/src/main/java/net/yiyuan/service/SysAreaAppService.java
+++ yiyuan-sys/yiyuan-sys-app-service/src/main/java/net/yiyuan/service/SysAreaAppService.java
@@ -2,6 +2,9 @@ package net.yiyuan.service;
 
 import icu.mhb.mybatisplus.plugln.base.service.JoinIService;
 import net.yiyuan.model.SysArea;
+import net.yiyuan.vo.SysAreaQueryVO;
+
+import java.util.List;
 
 /**
  * 行政区域移动端Service层接口
@@ -9,4 +12,6 @@ import net.yiyuan.model.SysArea;
  * @author 一源-花和尚
  * @date 2023-09-18
  */
-public interface SysAreaAppService extends JoinIService<SysArea> {}
+public interface SysAreaAppService extends JoinIService<SysArea> {
+    List<SysAreaQueryVO> getAreaTree(String pid) throws Exception;
+}
